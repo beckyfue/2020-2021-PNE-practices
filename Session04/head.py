80bc544bedc6ecd06a7e65e21c9d3374f204e9c6
beckyfue <r.fuentesa.2020@alumnnos.urjc.es>
2021-02-24 18:52:12 +0100

    Practice 0 session 1

--- Session04/head.py
+++ Session04/head.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-FILENAME = "RNU6_269P.txt"
+FILENAME = "../P0/sequences/RNU6_269P.txt"
 file_contents = Path(FILENAME).read_text()
 line = file_contents.split("\n")
 print(line[0])
